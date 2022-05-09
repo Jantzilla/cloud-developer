d51a33199bb4a2e51d4406119f17c774e39fca73
Jantz <jantzillac.93@gmail.com>
2022-05-09 01:04:10 -0500

    Add update todo handler

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -4,7 +4,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { updateTodo } from '../../businessLogic/todos'
+import { updateTodo } from '../../helpers/todos'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 import { getUserId } from '../utils'
 
@@ -14,14 +14,20 @@ export const handler = middy(
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
     // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
 
+    await updateTodo(updatedTodo, getUserId(event), todoId)
 
-    return undefined
-)
+    return {
+      statusCode: 200,
+      body: ''
+    }
+
+})
 
 handler
   .use(httpErrorHandler())
   .use(
     cors({
+      origin: '*',
       credentials: true
     })
   )
