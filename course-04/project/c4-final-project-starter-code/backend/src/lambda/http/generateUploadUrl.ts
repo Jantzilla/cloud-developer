13b561eead2b378bed20fa1e9da311fc57064b84
Jantz <jantzillac.93@gmail.com>
2022-05-09 01:02:32 -0500

    Create upload url handler

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/generateUploadUrl.ts
@@ -4,16 +4,22 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
+import { createAttachmentPresignedUrl } from '../../helpers/todos'
+// import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    
 
-    return undefined
+    const uploadUrl = await createAttachmentPresignedUrl(todoId)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        uploadUrl: uploadUrl
+      })
+    }
   }
 )
 
@@ -21,6 +27,7 @@ handler
   .use(httpErrorHandler())
   .use(
     cors({
+      origin: '*',
       credentials: true
     })
   )
